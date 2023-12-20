86fd18c67700eab4bdc9fff1acdeab8391cf7184
Jakub Kanna <hello.jakubkanna@gmail.com>
2023-12-20 15:18:23 +0100

    removed iterate, added at,pop, contains, find - base done

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 // [ NODE(head) ] -> [ NODE ] -> [ NODE(tail) ] -> null
 
 class LinkedList {
-  //for readability
-  iterateList(callback) {
-    let current = this.listHeadItem;
-    while (current !== null) {
-      callback(current);
-      current = current.next;
-    }
-  }
-
   constructor() {
     this.listHeadItem = null;
     this.listSize = 0;
@@ -41,40 +32,90 @@ class LinkedList {
   // returns the total number of nodes in the list
   get size() {
     let count = 0;
+    let current = this.listHeadItem;
 
-    while (this.listHeadItem.next !== null) {
+    while (current !== null) {
       count++;
-      this.listHeadItem = this.listHeadItem.next;
+      current = current.next;
     }
     return count;
   }
 
   // returns the first node in the list
   get head() {
-    return this.listHeadItem;
+    return this.listHeadItem.data;
   }
 
   // returns the last node in the list
   get tail() {
-    while (this.listHeadItem.next !== null) {
-      this.listHeadItem = this.listHeadItem.next;
+    let current = this.listHeadItem;
+
+    while (current.next !== null) {
+      current = current.next;
     }
-    return this.listHeadItem.data;
+    return current.data;
   }
 
   // returns the node at the given index
-  at(index) {}
+  at(index) {
+    let current = this.listHeadItem;
+
+    for (let i = 0; i < index; i++) {
+      if (current === null) {
+        return null; // Index out of bounds
+      }
+      current = current.next;
+    }
+    return current.data;
+  }
 
   //removes the last element from the list
-  pop() {}
+  pop() {
+    if (!this.listHeadItem) return null; // List is empty
+
+    if (!this.listHeadItem.next) {
+      // If there is only one element, remove it
+      const poppedValue = this.listHeadItem.data;
+      this.listHeadItem = null;
+      return poppedValue;
+    }
+
+    let current = this.listHeadItem;
+    let previous = null;
+
+    while (current.next !== null) {
+      previous = current;
+      current = current.next;
+    }
+
+    previous.next = null;
+  }
 
   //returns true if the passed in value is in the list and otherwise returns false.
-  contains(value) {}
+  contains(value) {
+    let current = this.listHeadItem;
+    while (current != null) {
+      if (current.data === value) return true;
+      current = current.next;
+    }
+    return false;
+  }
 
   //returns the index of the node containing value, or null if not found.
-  find(value) {}
+  find(value, current = this.listHead, index = 0) {
+    if (current === null) {
+      return null; // Return null if list is empty
+    }
 
-  // prints LinkedList in console, The format should be: ( value ) -> ( value ) -> ( value ) -> null
+    if (current.value === value) {
+      return index; // Return the current index if there's a match
+    }
+
+    // Recursive case: Move to the next node and increment the index
+    return this.find(value, current.nextNode, index + 1);
+  }
+
+  // prints LinkedList in console, the format should be: ( value ) -> ( value ) -> ( value ) -> null
   toString() {
     let result = "";
     let current = this.listHeadItem;
@@ -88,7 +129,6 @@ class LinkedList {
     return result;
   }
 
-  //extra
   // that inserts a new node with the provided value at the given index
   insertAt(value, index) {}
 
@@ -103,7 +143,7 @@ class Node {
   }
 }
 
-//
+//logs
 
 const list = new LinkedList();
 
@@ -118,9 +158,19 @@ const [item1, item2, item3, item4, item5] = [
 list.prepend(item1);
 list.append(item2);
 list.append(item3);
+list.prepend(item4);
+list.append(item5);
 
-console.log(list.toString());
-
-console.log(list.size);
-
-console.log(list.tail);
+console.table({
+  list: list.toString(),
+  size: list.size,
+  tail: list.tail,
+  head: list.head,
+  ["at 1"]: list.at(1),
+});
+console.table({
+  "list after pop": list.toString(),
+  "contains John?": list.contains("John"),
+  "find Abraham": list.contains("Abraham"),
+});
+console.log(list.insertAt(null, 2));
